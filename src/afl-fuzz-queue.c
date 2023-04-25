450313d71373d62bbdfaf80c603abbd60bdf96bd
njuQRX <qrx@smail.nju.edu.cn>
2023-04-25 15:19:30 +0800

    Set round to 0 to first prioritization

--- src/afl-fuzz-queue.c
+++ src/afl-fuzz-queue.c
@@ -1585,7 +1585,7 @@ void dist_seed_prioritize(afl_state_t *afl) {
 
   // Log
   fprintf(dist->log_fp, "prior_round %u, total_time %llu, cal_time %llu, sort_time %llu\n",
-          ++dist->log_cnt, total_time, ((cal_complete_time - start_time) / 1000),
+          dist->log_cnt++, total_time, ((cal_complete_time - start_time) / 1000),
           ((sort_complete_time - cal_complete_time) / 1000));
 
 }
